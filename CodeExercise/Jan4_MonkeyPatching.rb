7da851b06db8b4f4dd380c6a3abd4dad8eb3280e
Eric Ash <eric@gaglani.com>
2017-10-13 13:04:50 -0400

    Jan 4 exercise

--- CodeExercise/Jan4_MonkeyPatching.rb
+++ CodeExercise/Jan4_MonkeyPatching.rb
@@ -1,6 +1,36 @@
 require 'rspec'
 
+module RubyContent
+  refine String do 
+    def commentize
+      "# #{self}"
+    end
+  end
+end
+
+module HtmlContent
+  refine String do 
+    def commentize
+      "<-- #{self} -->"
+    end
+  end
+end
+
 class ContentController
+  using RubyContent
+
+  def initialize(word)
+    @word = word
+  end
+
+  def hidden_content
+    @word.commentize
+  end
+end
+
+class HtmlController
+  using HtmlContent
+
   def initialize(word)
     @word = word
   end
