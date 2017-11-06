4c87430a39385d49cec7841b3e1fd7d698dc7007
Eric Ash <eric@gaglani.com>
2017-11-05 23:18:33 -0500

    Jan6 code exercise

--- CodeExercise/Jan6 _AddNextStringSeq.rb
+++ CodeExercise/Jan6 _AddNextStringSeq.rb
@@ -7,6 +7,12 @@ def increment_value str
 	arr.join
 end
 
+## video solution
+
+def increment_value1 str
+	str + str.next.slice(-1)
+end
+
 describe 'Increment string value sequence' do
   it 'appends the next item in the sequence of a string' do
     expect(increment_value 'abcde').to eq('abcdef')
